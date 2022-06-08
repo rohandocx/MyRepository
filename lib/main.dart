e4aae283b2b1643bc67a01001e47bf0f831cf92a
rohandocx <rohan2056.be20@chitkara.edu.in>
2022-06-08 18:34:58 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+
+   
     return MaterialApp(
-        home: Material(
-      child: Center(
-          child: Container(
-        child: const Text("Welcome to Flutter!"),
-      )),
-    ));
+        home:HomePage(),
+    );
   }
 }
